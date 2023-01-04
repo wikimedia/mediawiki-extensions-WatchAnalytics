19b42f8f4b348e78fd6e1531d1c31244f399c1b8
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2023-01-04 23:41:13 +0530

    Add Grunt testing and remove travis configurations
    Change-Id: I6a82e9282ac751cec6573aa8cb260f999914175b

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+/* eslint-env node */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+
+	grunt.initConfig( {
+		eslint: {
+			options: {
+				cache: true
+			},
+			all: [
+				'**/*.{js,json}',
+				'!node_modules/**',
+				'!vendor/**',
+				'!modules/**'
+			]
+		},
+		banana: {
+			all: 'i18n/'
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'eslint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
--- build/travis/run-tests.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-set -ex
-
-BASE_PATH=$(pwd)
-MW_INSTALL_PATH=$BASE_PATH/../mw
-
-## just doing trivial script for now, no tests. Want to make Travis not throw errors each commit.
