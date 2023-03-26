c3d493d0f6c00fcb1d1f9c836e44d0263d2dd9cf
Yaron Koren <yaron57@gmail.com>
2023-03-26 15:02:56 +0000

    Remove irrelevant stuff from top of PendingApproval.php
    Change-Id: Ic78f6641789838b5ce6527bb34a68022bea5f426

--- includes/PendingApproval.php
+++ includes/PendingApproval.php
@@ -1,20 +1,4 @@
 <?php
-/**
- * MediaWiki Extension: WatchAnalytics
- * http://www.mediawiki.org/wiki/Extension:WatchAnalytics
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * This program is distributed WITHOUT ANY WARRANTY.
- */
 
 /**
  *
@@ -23,15 +7,6 @@
  * @author James Montalvo
  * @license MIT
  */
-
-# Alert the user that this is not a valid entry point to MediaWiki if they try to access the special pages file directly.
-if ( !defined( 'MEDIAWIKI' ) ) {
-	echo <<<EOT
-To install this extension, put the following line in LocalSettings.php:
-require_once( "$IP/extensions/WatchAnalytics/WatchAnalytics.php" );
-EOT;
-	exit( 1 );
-}
 
 class PendingApproval extends PendingReview {
 	public function __construct( $row, Title $title ) {
