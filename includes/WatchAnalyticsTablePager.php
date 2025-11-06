37092c75275ec8ab0f00db41a7085280f383b259
Yaron Koren <yaron57@gmail.com>
2025-11-06 19:52:17 +0000

    Replace now-removed Xml::submitButton()
    Change-Id: Id3d2626059357194e63a0e6f24cb89d76b53a680
    (cherry picked from commit b0f858c46c41e7d488cf4d4a4917d9eaf2ff1502)

--- includes/WatchAnalyticsTablePager.php
+++ includes/WatchAnalyticsTablePager.php
@@ -252,7 +252,7 @@ abstract class WatchAnalyticsTablePager extends TablePager {
 			'<tr>
 				<td></td>
 				<td>' .
-			Xml::submitButton( $this->msg( 'allmessages-filter-submit' )->text() ) .
+			Html::submitButton( $this->msg( 'allmessages-filter-submit' )->text() ) .
 			"</td>\n
 			</tr>" .
 
